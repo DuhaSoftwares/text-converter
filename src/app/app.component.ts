ff20def71ad51b23bdcab1f912c3837734e82001
Musaib <musaib709@outlook.com>
2025-02-11 22:28:32 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import {  Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TopNavComponent } from './components/internal/top-nav/top-nav.component';
+import { FooterComponent } from './components/internal/footer/footer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule, CommonModule, ReactiveFormsModule,TopNavComponent]
+  imports: [RouterModule, CommonModule, ReactiveFormsModule,TopNavComponent,FooterComponent]
 })
 export class AppComponent implements OnInit {
   title = 'text-editor';
---
+++ src/app/components/internal/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared.module';
+
+@Component({
+  selector: 'app-footer',
+  imports: [SharedModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
